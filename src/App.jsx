b991b0b3064a8bdc20a4c363d6dda7587fa83f79
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-03-14 19:04:39 +0600

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Countries from './Components/Countries/Countries'
+
+function App() {
+
+  return (
+    <div>
+    <Countries></Countries>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+  return (
+    <div>
+      <h1>In The Countries</h1>
+    </div>
+  );
+};
+
+export default Countries;
